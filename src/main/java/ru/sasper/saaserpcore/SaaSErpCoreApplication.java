bef44d7d498ef906e29d18fd7eb9bbd4772d8163
defix-dev <kostikmelnik7778@gmail.com>
2026-06-11 18:54:30 +0300

    init saas erp core (creating spring project, database scheme in luquibase)

---
+++ src/main/java/ru/sasper/saaserpcore/SaaSErpCoreApplication.java
@@ -0,0 +1,13 @@
+package ru.sasper.saaserpcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaSErpCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaaSErpCoreApplication.class, args);
+    }
+
+}
